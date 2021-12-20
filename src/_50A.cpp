cd9457d204476e9bf6d7c11a40809bf1912a1f78
MXLev <skuratov.lev@gmail.com>
2021-12-20 20:15:47 +0600

    158A

---
+++ src/_50A.cpp
@@ -0,0 +1,13 @@
+//
+// https://codeforces.com/problemset/problem/50/A
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << (n * m) / 2;
+}
