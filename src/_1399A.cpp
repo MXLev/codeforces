744a2b828a4f6575ec4c6f768cbd2e791211fb6f
MXLev <skuratov.lev@gmail.com>
2022-01-28 18:37:53 +0600

    1399A

--- src/_1399A.cpp
+++ src/_1399A.cpp
@@ -15,7 +15,7 @@ int main() {
         int n;
         cin >> n;
         if (n == 1){
-            cout << "YES";
+            cout << "YES" << '\n';
             continue;
         }
         vector<int> arr(n);
@@ -25,7 +25,9 @@ int main() {
         sort(arr.begin(), arr.end());
         bool breaked = true;
         for (int i = 0; i < n - 1; ++i) {
-            if (arr[i] + 1 == arr[i + 1] || arr[i] == arr[i + 1]){
+            if (arr[i] + 1 == arr[i + 1]){
+                breaked = false;
+            } else if (arr[i] == arr[i + 1]){
                 breaked = false;
             } else {
                 breaked = true;
