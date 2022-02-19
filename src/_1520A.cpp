2c10a4801e31fc3e33ade41a5c662796852136e2
MXLev <skuratov.lev@gmail.com>
2022-02-19 14:08:28 +0600

    1520A

--- src/_1520A.cpp
+++ src/_1520A.cpp
@@ -19,16 +19,32 @@ int main() {
         for (int i = 0; i < n; ++i) {
             cin >> arr[i];
         }
-        for (int i = 0; i < n; ++i) {
-            if (i > 0 && find(arr.begin(), arr.end(), arr[i]) == arr.end() && arr[i - 1] != arr[i]){
-                cout << "NO" << '\n';
-                flag = true;
+        for (int i = 1; i < arr.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (arr[j] == arr[i] && j + 1 != i){
+                    bool flag2 = false;
+                    for (int k = j; k < i - 1; ++k) {
+                        if (arr[k] == arr[k + 1]){
+                            flag2 = true;
+                        } else {
+                            flag2 = false;
+                            break;
+                        }
+                    }
+                    if (!flag2){
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if (flag){
                 break;
             }
         }
         if (flag){
-            continue;
+            cout << "NO" << '\n';
+        } else {
+            cout << "YES" << '\n';
         }
-        cout << "YES" << '\n';
     }
 }
