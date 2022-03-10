ce5e63573e8426874288516d64d323b716776b50
MXLev <skuratov.lev@gmail.com>
2022-03-10 20:21:07 +0600

    630A

---
+++ src/_630A.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Beast on 10.03.2022.
+// https://codeforces.com/problemset/problem/630/A
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    cout << 25;
+}
