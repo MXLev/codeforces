1842ab61b13148985aaa0d86d659cb4e058c8fe1
MXLev <skuratov.lev@gmail.com>
2022-03-19 20:36:21 +0600

    1311A

---
+++ src/_1311A.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Beast on 19.03.2022.
+// https://codeforces.com/problemset/problem/1311/A
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int y, w;
+    cin >> w >> y;
+    //6-max(w,y)
+}
