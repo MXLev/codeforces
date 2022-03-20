19df2d8f471cb4cf2ebd9f50756a819ae7390fc0
MXLev <skuratov.lev@gmail.com>
2022-03-20 20:53:08 +0600

    1294A

---
+++ src/_1294A.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Beast on 20.03.2022.
+// https://codeforces.com/problemset/problem/1294/A
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+}
