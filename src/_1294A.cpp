d9807321ac1ee710dbfe8632c32289cc52fd7bc9
MXLev <skuratov.lev@gmail.com>
2022-03-28 20:36:52 +0600

    1294A

--- src/_1294A.cpp
+++ src/_1294A.cpp
@@ -4,9 +4,33 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main() {
-
+    int t;
+    cin >> t;
+    for (int q = 0; q < t; ++q) {
+        vector<int> sisters(3);
+        int n;
+        cin >> sisters[0] >> sisters[1] >> sisters[2] >> n;
+        sort(sisters.rbegin(), sisters.rend());
+        n -= sisters[0] - sisters[1];
+        n -= sisters[0] - sisters[2];
+        sisters[1] = sisters[2] = sisters[0];
+        if (sisters[0] == sisters[1] && sisters[0] == sisters[2] && n == 0) {
+            cout << "YES" << '\n';
+            continue;
+        }
+        if (n <= 0){
+            cout << "NO" << '\n';
+            continue;
+        }
+        if (sisters[0] == sisters[1] && sisters[0] == sisters[2] && n % 3 == 0){
+            cout << "YES" << '\n';
+        } else {
+            cout << "NO" << '\n';
+        }
+    }
 }
