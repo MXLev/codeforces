d954d15ff679379fd700afd4120730eef3fd9edf
MXLev <skuratov.lev@gmail.com>
2022-05-01 16:25:37 +0600

    mfti

--- src/_mftiC.cpp
+++ src/_mftiC.cpp
@@ -8,69 +8,6 @@ using namespace std;
 
 int w = 0, h = 0, xe = 0, ye = 0;
 
-//int find(int rp, int cp, int r, int c, int count){
-//    if (count > max(w, h)) {
-//        return INT32_MAX;
-//    }
-//
-//    if (r < 1){
-//        r = h;
-//    } else if (r > h){
-//        r = 1;
-//    }
-//    if (c < 1){
-//        c = w;
-//    } else if (c > w){
-//        c = 1;
-//    }
-//    if (r == ye && c == xe){
-//        return count;
-//    }
-//
-//    int result1 = INT32_MAX, result2 = INT32_MAX, result3 = INT32_MAX, result4 = INT32_MAX;
-//    int r1 = 0, c1 = 0;
-//
-//    if (r == ye) {
-//        result1 = abs(xe - c);
-//        result2 = (w - xe) + c;
-//
-//        return count + min(result1, result2);
-//    }
-//
-//    if (c == xe) {
-//        result1 = abs(ye - r);
-//        result2 = (h - ye) + r;
-//
-//        return count + min(result1, result2);
-//    }
-//
-//    r1 = r + 1;
-//    c1 = c;
-//    if (r1 != rp || c1 != cp) {
-//        result1 = find(r, c, r1, c1, count + 1);
-//    }
-//
-//    r1 = r - 1;
-//    c1 = c;
-//    if (r1 != rp || c1 != cp) {
-//        result2 = find(r, c, r1, c1, count + 1);
-//    }
-//
-//    r1 = r;
-//    c1 = c + 1;
-//    if (r1 != rp || c1 != cp) {
-//        result3 = find(r, c, r1, c1, count + 1);
-//    }
-//
-//    r1 = r;
-//    c1 = c - 1;
-//    if (r1 != rp || c1 != cp) {
-//        result4 = find(r, c, r1, c1, count + 1);
-//    }
-//
-//    return min(min(result1, result2), min(result3, result4));
-//}
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -78,16 +15,21 @@ int main() {
     int x0, y0;
     cin >> x0 >> y0 >> xe >> ye;
 
-    int dxdy = abs(xe - x0) + abs(ye - y0);
-    int dxwy = abs(xe - x0) + (h - ye) + y0;
-    int wxdy = (w - xe) + x0 + abs(ye - y0);
-    int wxwy = (w - xe) + x0 + (h - ye) + y0;
-    cout << min(min(dxdy, dxwy), min(wxdy, wxwy));
+    int dx = abs(xe - x0);
+    if (x0 > xe){
+        int t = x0;
+        x0 = xe;
+        xe = t;
+    }
+    int wx = (x0 - 1) + (w - xe + 1);
 
-//    int r1 = find(y0, x0, y0 + 1, x0, 1),
-//        r2 = find(y0, x0, y0 - 1, x0, 1),
-//        r3 = find(y0, x0, y0, x0 + 1, 1),
-//        r4 = find(y0, x0, y0, x0 - 1, 1);
+    int dy = abs(ye - y0);
+    if (y0 > ye){
+        int t = y0;
+        y0 = ye;
+        ye = t;
+    }
+    int wy = (y0 - 1) + (h - ye + 1);
 
-//    cout << min(min(r1, r2), min(r3, r4)) << "\n";
+    cout << min(dx, wx) + min(dy, wy) << '\n';
 }
---
+++ src/_mftiD.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Beast on 01.05.2022.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int find (int r, int c, int sr, int sc, vector<vector<char>> &screen, string &pattern) {
+    int counter = 0;
+
+    for (int rr = r; rr < sr; ++rr) {
+        for (int cc = c; cc < sc; ++cc) {
+            if (pattern.find(screen[rr][cc]) != string::npos) {
+                ++counter;
+            }
+        }
+    }
+
+    return counter;
+}
+
+int main() {
+    int u, d, m;
+    string pattern;
+    cin >> u >> d >> m;
+    cin >> pattern;
+    vector<vector<char>> screen(u, vector<char> (d));
+    for (int r = 0; r < u; ++r) {
+        for (int c = 0; c < d; ++c) {
+            cin >> screen[r][c];
+        }
+    }
+
+    int maxBad = 0, maxSquare = 1;
+    double maxRate = 0.0;
+
+    for (int r = 0; r < (u - m); ++r) {
+        for (int c = 0; c < (d - m); ++c) {
+
+            for (int sr = m; sr < u; ++sr) {
+                for (int sc = m; sc < d; ++sc) {
+
+                    int bad = find(r, c, sr, sc, screen, pattern);
+                    int square = ((sr - r) * (sc + c));
+                    double rate = (double)bad / square;
+                    if (maxRate < rate) {
+                        maxRate = rate;
+                        maxSquare = square;
+                        maxBad = bad;
+                        cout << maxRate << ' ' << maxSquare << ' ' << maxBad << '\n';
+                        cout << r << ' ' << sr << ' ' << c << ' ' << sc << '\n' << "\n";
+                    }
+                }
+            }
+        }
+    }
+
+    cout << maxBad << "/" << maxSquare << "\n";
+}
