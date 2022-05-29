e764d02713a6dd9263d2f0b46045021322e5f306
MXLev <skuratov.lev@gmail.com>
2022-05-29 20:16:44 +0600

    1293

---
+++ src/1293.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Beast on 29.05.2022.
+// https://acm.timus.ru/problem.aspx?space=1&num=1001
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << n * a * b * 2;
+}
