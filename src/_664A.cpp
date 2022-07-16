54d32c2cebcc76a24c4dfe8a19821eb9b8fd9837
MXLev <skuratov.lev@gmail.com>
2022-07-16 20:03:18 +0600

    _664A

---
+++ src/_664A.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Beast on 16.07.2022.
+// https://codeforces.com/problemset/problem/664/A
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    if (a != b){
+        cout << 1 << '\n';
+    } else {
+        cout << a << '\n';
+    }
+}
